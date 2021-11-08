7c23bc2ef4bf7738b8fd0dff8fcaf0b23a3854aa
kylixalex <kylixsilva@gmail.com>
2021-11-08 16:15:40 +0000

    Add utils

---
+++ utils.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+void	ourPutChar(char c)
+{
+	write(1, &c, 1);
+}
+
+int		ourStrLen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+	{
+		len++;
+	}
+	return (len);
+}
+
+void	ourPutString(char *str)
+{
+	write(1, str, ourStrLen);
+}
---
+++ utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+void	ourPutChar(char c);
+int		ourStrLen(char *str);
+void	ourPutString(char *str);
+
+#endif
