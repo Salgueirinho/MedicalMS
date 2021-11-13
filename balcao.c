cdb77934ef9f3b5358f3a463d0e1d7187e683e6b
kylixalex <kylixsilva@gmail.com>
2021-11-13 21:30:32 +0000

    Add functional unnamed pipe from balcao to classificador. This marks the finish of the first meta. Hurray! :)

--- balcao.c
+++ balcao.c
@@ -1,7 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "utils.h"
 #include "balcao.h"
+#include <unistd.h>
+#include <sys/wait.h>
+#include "utils.h"
+
+int	callClassificador(char *sintomas)
+{
+	int	fd[2];
+	int	pid1;
+	int	pid2;
+
+	if (pipe(fd) == -1)
+	{
+		return -1;
+	}
+
+	pid1 = fork();
+	if (pid1 < 0)
+	{
+		return -2;
+	}
+	else if (pid1 == 0)
+	{
+		dup2(fd[1], 1);
+		close(fd[0]);
+		close(fd[1]);
+		write(1, sintomas, ourStrLen(sintomas));
+		write(1, "\n", 1);
+		write(1, "#fim", 4);
+		write(1, "\n", 1);
+	}
+
+	pid2 = fork();
+	if (pid2 < 0)
+	{
+		return -3;
+	}
+	else if (pid2 == 0)
+	{
+		dup2(fd[0], 0);
+		close(fd[0]);
+		close(fd[1]);
+		execl("./classificador", "./classificador", NULL);
+	}
+	
+	close(fd[0]);
+	close(fd[1]);
+	waitpid(pid2, NULL, 0);
+}
 
 int	getNumberFromEnv(char *env_name)
 {
@@ -22,10 +71,13 @@ int	getNumberFromEnv(char *env_name)
 	return -1;
 }
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
-	char	comando[20];
 	ValoresMaximos valoresMaximos;
+	char	comando[20];
+	int		i;
+
+	i = 1;
 
 	valoresMaximos.max_clientes = getNumberFromEnv("MAXCLIENTES");
 	if (valoresMaximos.max_clientes <= 0)
@@ -35,10 +87,30 @@ int	main(void)
 	if (valoresMaximos.max_medicos <= 0)
 		return -2;
 
+	// Primeira meta
+	if (argc < 2)
+		ourPutString("Nao foram indicados sintomas\n");
+	else
+	{
+		while (i < argc)
+		{
+			ourPutString("Para os sintomas introduzidos:\n - ");
+			ourPutString(argv[i]);
+			ourPutString("\nDeduzimos a especialidade e urgencia:\n - ");
+			callClassificador(argv[i]);
+			i++;
+		}
+	}
+	/*
+	
+	-- So na segunda meta! --
+	
   while(strcmp(comando, "encerra"))
 	{
     fflush(stdout);
     scanf("%20s", comando);
   }
+
+	*/
 	return 0;
 }
