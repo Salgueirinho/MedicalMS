1fb3fcb7352018d9c53e3fcd87d167806004d275
kylixalex <kylixsilva@gmail.com>
2021-11-13 21:14:36 +0000

    Add struct for max values

--- balcao.h
+++ balcao.h
@@ -1,6 +1,10 @@
 #ifndef BALCAO_H
 #define BALCAO_H
 
-#define MAX_FILA 5 // MAX UTENTES POR FILA POR ESPECIALIDADE
+typedef struct {
+	int	max_clientes;
+	int	max_medicos;
+	#define MAX_FILA 5 // MAX UTENTES POR FILA POR ESPECIALIDADE
+} ValoresMaximos, *pValoresMaximos;
 
 #endif // BALCAO_H
