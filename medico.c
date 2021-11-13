11132f837fd72a153e0355a3d1d542d5502b74d1
kylixalex <kylixsilva@gmail.com>
2021-11-13 16:11:48 +0000

    Add main arguments for medico

--- medico.c
+++ medico.c
@@ -1,4 +1,28 @@
-int	main(void)
+#include <stdlib.h>
+#include "medico.h"
+#include "utils.h"
+
+void	guardaNome(char* nome, pMedico medico){
+	medico->nomeMedico = nome;
+}
+
+void	guardaEspecialidade(char *especialidade, pMedico medico)
 {
+	medico->especialidade = especialidade;
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		ourPutString("Faltam argumentos de entrada: necessario nome e especialidade do Medico.\n");
+		exit(-1);
+	}
+	else
+	{
+		Medico especialista;
+		guardaNome(argv[1], &especialista);
+		guardaEspecialidade(argv[2], &especialista);
+	}
 	return 0;
 }
--- medico.h
+++ medico.h
@@ -1,4 +1,9 @@
 #ifndef MEDICO_H
 #define MEDICO_H
 
+typedef struct {
+  char *nomeMedico;
+  char *especialidade;
+} Medico, *pMedico;
+
 #endif // MEDICO_H
